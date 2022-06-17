7abf5fb57f09ea402f73bfbfde46430cb28c7e87
pyarosh <peteryarosh@gmail.com>
2022-06-17 13:28:40 -0400

    add path.extname

--- src/path.ts
+++ src/path.ts
@@ -38,3 +38,21 @@ export const dirname = (
 	() => path.dirname(p),
 	(reason: unknown) => toError(reason, "Unexpected error getting dirname")
 );
+
+/**
+ * Gets the extension of the path,
+ * from the last occurrence of the . (period) character to end of string in the last portion of the path.
+ * If there is no . in the last portion of the path,
+ * or if there are no . characters other than the first character of the basename of path,
+ * an empty string is returned.
+ * NOTE: A TypeError is thrown if path is not a string.
+ * 
+ * @param p The path to evaluate.
+ * @returns The extension of the path.
+ */
+export const extname = (
+	p: string
+): IOEither<Error, string> => tryCatch(
+	() => path.extname(p),
+	(reason: unknown) => toError(reason, "Unexpected error getting extname")
+);
